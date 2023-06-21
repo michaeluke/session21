14d92d8ba35c710e6dc3b04a69eeb3b8c0af517b
michaeluke <65449279+michaeluke@users.noreply.github.com>
2023-06-21 22:42:16 +0300

    .

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState , useContext } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {CardsTheme, ThemeContext} from './Theme'
 export default function Search(props) {
 
   const [searchTitle, setSearchTitle] = useState('');
   const [searchLocation, setSearchLocation] = useState('');
-  
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   const handleSearch = (event) => {
     props.get_data(searchTitle,searchLocation);
@@ -16,25 +17,25 @@ export default function Search(props) {
 
 
   return (
-    <div className='parent_search d-flex align-item-center justify-content-center'>
+    <div className={`parent_search d-flex align-item-center justify-content-center`}>
       
     <SearchIcon sx={{ color:'#939BF4', fontSize:'26px',position:'relative',left:"35px",top:"15px"}}/>
  
-    <input className='btn-title'
+    <input className={`btn-title ${theme}`}
           type="text"
           value={searchTitle}
           onChange={(event) => setSearchTitle(event.target.value)}
           placeholder='Filter by Title....'
         />
 
-    <input className='btn-filter'
+    <input className={`btn-filter ${theme}`}
           type="text"
           value={searchLocation}
           onChange={(event) => setSearchLocation(event.target.value)}
           placeholder='Filter by location..'
         />
 
-    <button onClick={handleSearch}> Search </button>
+    <button className="search_imp" onClick={handleSearch}> Search </button>
 
     <LocationOnIcon  sx={{ color:'#939BF4', fontSize:'26px',position:'relative',right:"350px",top:"13px"}}/>
     </div>
